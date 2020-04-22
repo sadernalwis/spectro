eeac8acafaceeacb11bebdbef02086405c41352f
Denesh <denesh.rajaratnam@gmail.com>
2020-04-23 03:56:12 +0530

    Added custom marker for hospitals

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -17,20 +17,24 @@ export class MapComponent implements OnInit, AfterViewInit {
 	@Output() selectedDistrict = new EventEmitter<string>();
 
 	_hospitals: Hospital[];
-	get hospitals(): Hospital[] {
-		return this._hospitals;
-	}
-
+	get hospitals(): Hospital[] { return this._hospitals; }
 	@Input('hospitals')
 	set hospitals(value: Hospital[]) {
 		this._hospitals = value;
 	}
+
+
+	hospitalMarkerOptions: google.maps.MarkerOptions;
 
 	constructor() { }
 
 	ngOnInit(): void {
 		this.center = { lat: 7.8774222, lng: 80.7003428 };
 		this.zoom = 8;
+
+		this.hospitalMarkerOptions = {
+			icon: "/assets/hospital-marker.png",
+		}
 	}
 
 	ngAfterViewInit(): void {
