977515f3b8fa02732d9b21467f3eb8f90108d2e5
Sadern Alwis <44372160+sadernalwis@users.noreply.github.com>
2020-05-20 23:03:30 +0530

    Revert "Add drone marker view to google map"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,27 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { Hospital } from './hospitals/hospital';
-import { SocketService } from './services/socket.service';
 
 @Component({
   selector: 'app-root',
-  // tslint:disable-next-line: indent
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    // tslint:disable-next-line: indent
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private cd: ChangeDetectorRef,
-    private socketService: SocketService
-  ) {
-    //  Initiating socket conncetion
-    socketService.createSocket();
-  }
-  selectedDistrict: string;
-  selectedHospitals: Hospital[];
-  selectedInstitutions: string[];
+	constructor(private cd: ChangeDetectorRef) { }
+		selectedDistrict: string;
+    selectedHospitals: Hospital[];
+    selectedInstitutions: string[];
 
-  changeSelectedDistrict($event: string) {
-    this.selectedDistrict = $event;
+	changeSelectedDistrict($event: string) {
+		this.selectedDistrict = $event;
 
-    // need this because google maps seems to mess with updates
+	// need this because google maps seems to mess with updates
     this.cd.detectChanges();
-  }
+    
+	}
 
-  updateHospitals($hospitals: Hospital[]) {
-    this.selectedHospitals = $hospitals;
-  }
+	updateHospitals($hospitals: Hospital[]) {
+		this.selectedHospitals = $hospitals;
+	}
 }
